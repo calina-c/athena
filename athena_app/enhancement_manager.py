a19a1138ae5dade896f0b8250bfb90e5aa04bdd3
Calina Tutunaru <calina.tutunaru@algotech.solutions>
2016-06-03 23:54:45 +0300

    Added related hashtags.

--- athena_app/enhancement_manager.py
+++ athena_app/enhancement_manager.py
@@ -1,5 +1,7 @@
 from cassandra.cluster import Cluster
 import uuid
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
 
 def enhance_h(key):
     cluster = Cluster()
@@ -16,17 +18,32 @@ def enhance_h(key):
     for r in res:
         harvest = r
 
-    # tweets = session.execute(
-    #     """
-    #     select * from tweet where history = %s
-    #     """,
-    #     (key, )
-    # )
+    tweets = session.execute(
+        """
+        select * from tweet where history = %s ALLOW FILTERING
+        """,
+        (key, )
+    )
+
+    tweet_texts = []
+    for tweet in tweets:
+        tweet_texts.append(tweet.content)
+
+    # stopwords = nltk.corpus.stopwords.words('english')
+
+    vectorizer = TfidfVectorizer(
+        min_df=10,
+        max_df=0.8,
+        sublinear_tf=True,
+        use_idf=True,
+        max_features=25,
+        token_pattern='#[a-zA-Z0-9][a-zA-Z0-9]*'
+    )
+
+    matrix = vectorizer.fit_transform(tweet_texts).toarray()
+    vocabulary = vectorizer.get_feature_names()
 
     return {
         'harvest': harvest,
-        'TestAnalysis': {
-            'test': 'test',
-            'test1': 'test1'
-        }
+        'vocabulary': vocabulary
     }
